eebb6c99321d542769a236a89a0b5cb7fb1ee871
Glaydetoledo <grtoledo98@gmail.com>
2024-10-15 11:49:36 -0300

    Create app01.py

---
+++ app01.py
@@ -0,0 +1,30 @@
+#criar uma variavél
+
+'''
+esse comentário pode
+ter multiplas
+e muitas linhas
+'''
+# criar uma variavel usando "snake_case"
+cor_da_casa = 'azul'
+
+# criar uma variável usando "camelCase"
+corDaCasa = "amarela"
+
+#exibir as coisas
+print("\n\n\n")
+print(cor_da_casa)
+print(corDaCasa)
+
+#criar uma variavel
+primeiro_nome = 'Glayde'
+segundo_nome = 'Toledo'
+
+#imprimir a varialvel
+
+print(primeiro_nome)
+
+print(segundo_nome)
+
+print(primeiro_nome,segundo_nome)
+
